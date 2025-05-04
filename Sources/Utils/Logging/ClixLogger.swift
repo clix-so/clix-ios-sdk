5596fcc9f4a6cec9639baafcd6942b92d0a11214
Minkyu Cho <minkyu@greyboxhq.com>
2025-05-04 16:14:01 +0900

    feat: update logger

--- Sources/Utils/Logging/ClixLogger.swift
+++ Sources/Utils/Logging/ClixLogger.swift
@@ -3,34 +3,33 @@ import Foundation
 class ClixLogger {
   private static var logLevel: ClixLogLevel = .info
 
-  init() {}
-
   static func setLogLevel(_ level: ClixLogLevel) {
     logLevel = level
   }
 
   static func log(level: ClixLogLevel, category: ClixLogCategory, message: String, error: Error? = nil) {
-    guard level.rawValue >= logLevel.rawValue else { return }
+    if level.rawValue < logLevel.rawValue {
+      return
+    }
 
     let timestamp = ISO8601DateFormatter().string(from: Date())
     let categoryString = String(describing: category)
-    let logMessage = "[\(timestamp)] [\(categoryString)] \(message)"
-
+    var logMessage = "[\(timestamp)] [\(categoryString)] \(message)"
     if let error = error {
-      print("ERROR: \(logMessage) - Error: \(error.localizedDescription)")
-    } else {
-      switch level {
-      case .debug:
-        print("DEBUG: \(logMessage)")
-      case .info:
-        print("INFO: \(logMessage)")
-      case .warn:
-        print("WARN: \(logMessage)")
-      case .error:
-        print("ERROR: \(logMessage)")
-      case .none:
-        break
-      }
+      logMessage += " - Error: \(error.localizedDescription)"
+    }
+
+    switch level {
+    case .debug:
+      print("DEBUG: \(logMessage)")
+    case .info:
+      print("INFO: \(logMessage)")
+    case .warn:
+      print("WARN: \(logMessage)")
+    case .error:
+      print("ERROR: \(logMessage)")
+    case .none:
+      break
     }
   }
 }
