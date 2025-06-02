d9459742a8e18c914f73cdb45adca07dc41195c7
JeongwooYoo <yoojw9607@gmail.com>
2025-06-02 19:23:07 +0900

    fix: update ClixPushNotificationPayload model

--- Sources/Models/ClixPushNotificationPayload.swift
+++ Sources/Models/ClixPushNotificationPayload.swift
@@ -3,13 +3,13 @@ import Foundation
 /// A model representing a Clix push notification payload
 public struct ClixPushNotificationPayload: Codable {
   public let messageId: String
-  public let landingUrl: String
-  public let imageUrl: String
+  public let landingUrl: String?
+  public let imageUrl: String?
 
   public init(
     messageId: String,
-    landingUrl: String,
-    imageUrl: String
+    landingUrl: String?,
+    imageUrl: String?
   ) {
     self.messageId = messageId
     self.landingUrl = landingUrl
