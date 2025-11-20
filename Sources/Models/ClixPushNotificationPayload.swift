cc8eaf075b54860b9d56dd4abfab1d7e1926c1b4
Alan Cho <minkyu@clix.so>
2025-11-20 22:36:22 +0900

    chore: remove redundant

--- Sources/Models/ClixPushNotificationPayload.swift
+++ Sources/Models/ClixPushNotificationPayload.swift
@@ -2,12 +2,8 @@ import Foundation
 
 struct ClixPushNotificationPayload: Codable {
   let messageId: String
-  let campaignId: String?
-  let userId: String?
-  let deviceId: String?
-  let trackingId: String?
-  let landingUrl: String?
   let imageUrl: String?
+  let landingUrl: String?
   let userJourneyId: String?
   let userJourneyNodeId: String?
 }
