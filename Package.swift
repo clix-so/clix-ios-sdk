bae4e99c85dd2e8665fae5443801aca1d43efbd6
Alan Cho <minkyu@clix.so>
2025-11-25 23:30:54 +0900

    feat: update project structure and dependencies for BasicApp sample

--- Package.swift
+++ Package.swift
@@ -14,27 +14,20 @@ let package = Package(
   ],
   dependencies: [
     .package(url: "https://github.com/firebase/firebase-ios-sdk.git", "10.0.0"..<"20.0.0"),
-    .package(url: "https://github.com/onepiece-studio/mmkv.git", from: "2.2.4")
+    .package(url: "https://github.com/onepiece-studio/mmkv.git", "1.3.0"..<"3.0.0"),
   ],
   targets: [
     .target(
       name: "Clix",
       dependencies: [
         .product(name: "FirebaseMessaging", package: "firebase-ios-sdk"),
-        .product(name: "MMKV", package: "mmkv")
+        .product(name: "MMKV", package: "mmkv"),
       ],
       path: "Sources",
       swiftSettings: [
         .define("DEBUG", .when(configuration: .debug)),
         .define("RELEASE", .when(configuration: .release)),
       ]
-    ),
-    .executableTarget(
-      name: "BasicApp",
-      dependencies: [
-        "Clix"
-      ],
-      path: "Samples/BasicApp/Sources"
     ),
     .testTarget(
       name: "ClixTests",
